bea8d4d43164e71340a36bb9025019a6d15498bf
Pablo Pirata <pablo@studocu.com>
2022-08-27 12:29:33 +0200

    start working on the backend logic

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,49 @@
+const nodeFetch = require('node-fetch');
+const { read, utils } = require('xlsx');
+
+const downloadSpreadsheetFile = async (spreadsheetId, sheetId = 0, forceCors = false) => {
+  let url = `https://docs.google.com/spreadsheets/d/${spreadsheetId}/export?format=xlsx&gid=${sheetId}`;
+  if (forceCors) {
+    url = `https://cors-anywhere.herokuapp.com/${url}`;
+  }
+
+  const response = await nodeFetch(url);
+  // eslint-disable-next-line no-return-await
+  return await response.blob();
+};
+
+const getGoogleFormData = async (googleFormId) =>
+    nodeFetch(`https://docs.google.com/forms/d/e/${googleFormId}/viewform?embedded=true`, {
+      method: 'GET',
+    }).then((response) => {
+      if (!response.ok) {
+        throw new Error('Network request failed');
+      }
+
+      return response.text().then((data) => {
+        let loadData = data.split('FB_PUBLIC_LOAD_DATA_');
+        loadData = loadData[1].split(';');
+        // eslint-disable-next-line no-new-func
+        const getLoadData = new Function(`const result${loadData[0]}; return result`);
+        // let shuffleSeed = data.split('data-shuffle-seed="');
+        // shuffleSeed = shuffleSeed[1].split('"');
+        return {
+          loadData: getLoadData(),
+          // shuffleSeed: shuffleSeed[0],
+        };
+      });
+    });
+
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
+  const googleFormsData = await getGoogleFormData(process.env.GATSBY_GOOGLE_FORM_ID);
+
   createPage({
     path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+    component: require.resolve("./src/templates/FormPage.jsx"),
+    context: {
+      googleFormsData,
+    },
+    // defer: true,
   })
 }
