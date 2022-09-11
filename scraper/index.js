c46f8cae2bdb7c14f924fc5b4485142313f43baf
Pablo Pirata <pablo@studocu.com>
2022-09-11 20:52:15 +0200

    remove usage of dotenv

--- scraper/index.js
+++ scraper/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
-require('dotenv').config({
-    path: path.resolve(__dirname, '..', '.env'),
-});
+// require('dotenv').config({
+//     path: path.resolve(__dirname, '..', '.env'),
+// });
 
 const { writeFileSync, mkdirSync, cpSync } = require('fs');
 
